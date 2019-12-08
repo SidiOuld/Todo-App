612287d7c73648cdad4ce89786da10eb1a21bb35
Sidi Ould <sidiould1@gmail.com>
2019-12-08 13:55:29 -0600

    added TodoList and styles

---
+++ src/Components/TodoList.js
@@ -0,0 +1,31 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+
+import React, {Component} from "react";
+import FlipMove from "react-flip-move";
+
+class TodoList extends Component{
+
+    delete(key){
+        this.props.delete(key);
+    }
+
+
+    render(){
+
+        return (
+            this.props.entries.map((item)=>{
+                return (
+                    <ul className="todoApp__lists ">
+                        <FlipMove duration={250} easing="ease-out">
+                       <li className="todoApp__lists--li" onClick={()=> this.delete(item.key)} key={item.key}>{item.text}</li>
+                       </FlipMove>
+                    </ul>
+                )
+            })
+        )
+    }
+};
+
+export default TodoList;
+
