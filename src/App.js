1d263c00abefdbab46bb20f8aff90735cc1a66fd
Sidi Ould <sidiould1@gmail.com>
2019-12-16 08:19:42 -0500

    removed dead code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import TodoForm from './Components/TodoForm';
 import "./index.css"
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div className="App">
